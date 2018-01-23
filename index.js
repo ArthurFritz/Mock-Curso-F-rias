eee004fc1732809cb5e22264302ff8a46a0a7db8
Arthur Fritz Santiago <arthur.santiago@matera.com>
2018-01-23 13:12:37 -0200

    Liberar cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PATH = "/api/v1/";
 app.options("*", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With');
   res.send(200);
 });
 
