c4d2a1eebe23bf70d46807a7deee0103fc365e73
Arthur Fritz Santiago <arthur.santiago@matera.com>
2018-01-23 12:56:44 -0200

    Liberar cors

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ var relatorio = require('./routes/relatorio');
 const PATH = "/api/v1/";
 
 app.options("*", function(req, res, next){
-  var origin = req.get('origin'); 
-  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.send(200);
